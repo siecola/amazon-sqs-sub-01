9e1deccfd6db96b09bdab1c287789e592a8cc3eb
Paulo César Siécola <paulo.cesar@br.brtoken>
2017-10-18 14:10:14 -0200

    Refactoring to use 'amazon-sqs-java-messaging-lib', version: '1.0.4'

--- src/main/java/com/siecola/aws/sqs/subscriber/config/JmsConfig.java
+++ src/main/java/com/siecola/aws/sqs/subscriber/config/JmsConfig.java
@@ -1,9 +1,12 @@
 package com.siecola.aws.sqs.subscriber.config;
 
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -17,14 +20,17 @@ import javax.jms.Session;
 @EnableJms
 public class JmsConfig {
 
-    SQSConnectionFactory connectionFactory =
-            SQSConnectionFactory.builder()
-                    .withRegion(Region.getRegion(Regions.US_WEST_2))
-                    //Comment this line below to use AWS SQS
-                    .withEndpoint("http://localhost:4576")
-                    .withAWSCredentialsProvider(new DefaultAWSCredentialsProviderChain())
-                    .build();
-
+    SQSConnectionFactory connectionFactory = new SQSConnectionFactory(
+            new ProviderConfiguration(),
+            AmazonSQSClientBuilder.standard()
+                    //Uncomment this line to use AWS SQS
+                    //.withRegion(Regions.US_WEST_2)
+                    //Comment this line to use AWS SQS
+                    .withEndpointConfiguration(
+                            new AwsClientBuilder.EndpointConfiguration("http://localhost:4576",
+                                    Regions.US_WEST_2.getName()))
+                    .withCredentials(new DefaultAWSCredentialsProviderChain())
+    );
 
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
